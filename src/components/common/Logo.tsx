bb401d608044dbd1ea4904031d039a0408b5b830
RaupscZAN <ra.upsc@edgeup.in>
2025-06-05 00:59:30 +0530

    Updated Logo.tsx

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -14,21 +14,11 @@ const Logo: React.FC<LogoProps> = ({ isWhite = false }) => {
   
   return (
     <div className="flex items-center">
-      {isWhite ? (
-        // For white version, use a white version of the logo directly
-        <img 
-          src="https://edgeup.in/wp-content/uploads/2024/03/edgeup-logo-white.png"
-          alt={siteName}
-          className="h-10"
-        />
-      ) : (
-        // For regular version, use the color logo
-        <img 
-          src={logoUrl}
-          alt={siteName}
-          className="h-10"
-        />
-      )}
+      <img 
+        src={logoUrl}
+        alt={siteName}
+        className={`h-10 ${isWhite ? 'brightness-0 invert' : ''}`}
+      />
     </div>
   );
 };
